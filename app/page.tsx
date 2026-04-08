35a7889549a1abaff26907701affb9601284941e
Saurav4359 <isauravsharmaokay4359@gmail.com>
2026-04-08 14:42:30 +0530

    Load featured events on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,43 @@
 import EventCard from "./components/EventCard";
 import ExploreBtn from "./components/ExploreBtn";
-import { EVENTS } from "../lib/constants";
-export default function Page() {
+import { IEvent } from "@/database/event.model";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+type EventsResponse = {
+  events: IEvent[];
+};
+
+export default async function Page() {
+  if (!BASE_URL) {
+    throw new Error("Missing NEXT_PUBLIC_BASE_URL environment variable.");
+  }
+
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events }: EventsResponse = await response.json();
+
   return (
-   <>
-  <section>
-    <h1 className="text-center">The Hub For Every Dev <br/> Even you Can't Miss </h1>
-    <p className="mt-5 text-center">Hackathons, Meetups, and Conferences, All In One Place</p>
-    <ExploreBtn/>
-    <div className="mt-20 space-y-7">
-      <h3>Featured Events</h3>
-      <ul className="events">
-        {
-          EVENTS.map((event)=> (
-            <li key={event.title}> <EventCard {... event}/></li>
-          ))
-        }
-      </ul>
-    </div>
-  </section>
-   </>
+    <>
+      <section>
+        <h1 className="text-center">
+          The Hub For Every Dev <br /> Even you Can't Miss
+        </h1>
+        <p className="mt-5 text-center">
+          Hackathons, Meetups, and Conferences, All In One Place
+        </p>
+        <ExploreBtn />
+        <div className="mt-20 space-y-7">
+          <h3>Featured Events</h3>
+          <ul className="events">
+            {events.length > 0 &&
+              events.map((event: IEvent) => (
+                <li key={event.title}>
+                  <EventCard {...event} />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </section>
+    </>
   );
 }
